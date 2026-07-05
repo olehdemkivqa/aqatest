5fb9b39f27c38ecce26f4c3ec3cd6d3a26a2a2ef
Oleh Demkiv <qa.oleh.demkiv@gmail.com>
2026-07-05 13:59:15 +0300

    Add global-teardown.js, changed reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,7 @@ dotenv.config( { path: './.env' } );
  */
 export default defineConfig({
   testDir: './tests',
+  globalTeardown: './global-teardown.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,7 +27,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     timeout: 45 * 1000,
