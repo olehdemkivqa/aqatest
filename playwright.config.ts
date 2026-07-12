e96ab95eabd8bfd66dc7d4673012f1353bb458cc
Oleh Demkiv <qa.oleh.demkiv@gmail.com>
2026-07-12 15:09:00 +0300

    workers+retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,13 +19,13 @@ export default defineConfig({
   testDir: './tests',
   globalTeardown: './global-teardown.ts',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
